0db414d75051fd6266697bf48ace995bb3be868b
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2021-04-09 21:13:13 +0200

    start profile page

---
+++ src/components/Profile/ProfileContainer.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="container">
+		<fa-icon class="container__feather" :icon="['fas', 'feather-alt']" />
+
+		<div class="container__general">
+			<div class="container__general__info">
+				<h2>{{ profile.user.username }}</h2>
+				<p>{{ profile.name }}</p>
+				<p>{{ profile.web }}</p>
+			</div>
+			<div class="container__general__avatar"></div>
+		</div>
+		<div class="container__bio">
+			<p>{{ profile.bio }}</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class ProfileContainer extends Vue {
+	private error: string = "";
+
+	//TODO: type
+	@Prop()
+	propProfile: any;
+	// propProfile: GeneralProfileInfo;
+
+	get profile() {
+		return this.propProfile;
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/general";
+
+.container {
+	&__general {
+		display: flex;
+	}
+
+	&__bio {
+	}
+}
+</style>
